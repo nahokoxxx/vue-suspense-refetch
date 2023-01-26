ad16ecb2df4248e66d9bbd4a8dd9890c0f70fda3
nahokoxxx <me@nahoko.dev>
2023-01-26 17:56:25 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Person from "./components/Person.vue";
+
+const ids = ref(["1", "5", "6"]);
+
+function change() {
+  ids.value = ["2", "3", "7"];
+}
+</script>
+
+<template>
+  <h1>The Star Wars People</h1>
+  <button @click="change">Change People</button>
+  <Suspense>
+    <template #default>
+      <div class="people">
+        <Person v-for="id in ids" :id="id" />
+      </div>
+    </template>
+    <template #fallback>
+      <div class="loading">Loading...</div>
+    </template>
+  </Suspense>
+</template>
+
+<style scoped>
+.people {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.loading {
+  margin-top: 30px;
+}
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+type Props = {
+  id: string;
+};
+
+const props = defineProps<Props>();
+
+const response = await fetch(`https://swapi.dev/api/people/${props.id}`);
+const person = await response.json();
+</script>
+
+<template>
+  <div>
+    <h2>{{ person.name }}</h2>
+    <div>Gender: {{ person.gender }}</div>
+    <div>Hair Color: {{ person.hair_color }}</div>
+  </div>
+</template>
